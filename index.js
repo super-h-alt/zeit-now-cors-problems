53b38a8bff802a2752bf82c43239d39ff3d4bb23
super-h-alt <54449084+super-h-alt@users.noreply.github.com>
2019-08-28 21:39:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config(); // require dotenv lib & use as: process.env.ENV_VAR
+
+// init required vars for project
+const express = require('express'), app = express(),
+	  bodyParser = require('body-parser'),
+	  helmet = require('helmet'),
+	  cors = require('cors');
+
+// Configure bodyparser to handle POST requests
+// a must-have for POST-requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// enable cors
+app.use(cors());
+app.options('/user/login', cors())
+
+app.use(helmet()); // use Helmet package for security to protect (https://github.com/helmetjs/helmet)
+
+app.post('/user/login', function(req, res){
+    res.status(200).json({ result: "error", response: req.body });
+});
+
+// app listen on port 3000
+app.listen(3000);
